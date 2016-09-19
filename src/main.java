d5a10ecea7f6af91cbb60eac021df708a34938b6
A.Sai Ram Sandeep <sandeep.a14@iiits.in>
2016-09-19 15:20:11 +0530

    oops class

--- src/main.java
+++ src/main.java
@@ -1,14 +1,13 @@
 
-import java.io.IOException;
 
 import org.apache.jena.ontology.OntClass;
 public class main {
   static String error;
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args) {
 		OntClass[] Classes=new OntClass[200];
 		// TODO Auto-generated method stub
 		owlparser OwlParser=new owlparser();
-		OwlParser.read("C:/Users/sandeep/Desktop/js game/type.rdf");
+		OwlParser.readfile("C:/Users/sandeep/Desktop/js game/type.rdf");
 		Classes=OwlParser.GetClasses();
 		
 		error=OwlParser.AddInstance(Classes[5], "http://www.co-ode.org/ontologies/pizza/pizza.owl#banu");
--- src/owlparser.java
+++ src/owlparser.java
@@ -1,34 +1,25 @@
 import java.io.FileWriter;
+import java.io.IOException;
 import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
-
 import org.apache.jena.rdf.model.ModelFactory;
-
-
 import org.apache.jena.util.iterator.ExtendedIterator;
 public final class owlparser {
-private OntModel base = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+private OntModel input = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
 private OntModel output = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
-
-   private  FileWriter out;
-   private  int i=0;
-   private OntClass[] Classes=new OntClass[200];
+private  FileWriter out;
+private  int i=0;
+private OntClass[] Classes=new OntClass[200];
   
-   public void read(String Value){
-	   try{
-   base.read(Value);
-   //output.read("C:/Users/sandeep/Desktop/js game/inst.rdf");
-   }catch(Error e)
-	   {
-		System.out.print("sssssssss");
-	   }
-	   }
- 
+public void readfile(String Value){
 	  
-   public OntClass[] GetClasses(){ 
-	   ExtendedIterator<OntClass> iter = base.listNamedClasses();
+   input.read(Value);
+   
+	   }
+public OntClass[] GetClasses(){ 
+	   ExtendedIterator<OntClass> iter = input.listNamedClasses();
 	   while ( iter.hasNext()){
 	  
 
@@ -45,9 +36,15 @@ private OntModel output = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM,
    public String  AddInstance(OntClass klass,String value){
 	   String fileName = "C:/Users/sandeep/Desktop/js game/output.rdf";
 	  
-	try {
-		out = new FileWriter( fileName );
-		Individual p1 = output.createIndividual( value, klass );
+	
+		try {
+			out = new FileWriter( fileName );
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+		    return "IOerror";
+		}
+		try {
+		Individual I1 = output.createIndividual( value, klass );
 
         output.write( out, "RDF/XML" );
 
